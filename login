db3c36ec8f5eb5f402d12b6c312c488e2833fcec
Samvid Dharanikota <samvid25@yahoo.com>
2017-02-05 21:00:29 +0530

    Add scripts

---
+++ login
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# replace /path/to/ with the location of login.py
+python /path/to/login.py
+
+
+
---
+++ login.py
@@ -0,0 +1,29 @@
+import sys
+from selenium import webdriver
+
+
+driver = webdriver.Firefox()
+
+try: 
+	driver.get("http://10.10.54.4:8090/")
+except:
+	sys.exit(0)
+
+print driver.title
+print "Logged in"
+
+
+#username field
+inputElement = driver.find_element_by_name("username")
+#Replace "staff" with your username/registration number
+inputElement.send_keys("staff")
+
+#password field
+inputElement = driver.find_element_by_name("password")
+#Replace "staff" with your password
+inputElement.send_keys("staff")
+
+
+driver.find_element_by_name("btnSubmit").click()
+
+driver.quit()
